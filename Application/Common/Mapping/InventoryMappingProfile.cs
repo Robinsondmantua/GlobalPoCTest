011fb8db5f4af0e32c3f47efd093837823cb38f7
Julio Gómez <julio.gomez@tuispain.com>
2022-04-15 15:16:07 +0200

    Automapper

---
+++ Application/Common/Mapping/InventoryMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.Inventory.Commands.Request;
+using Application.Features.Inventory.Dtos;
+using AutoMapper;
+using Domain.Aggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mapping
+{
+    /// <summary>
+    /// This class is used for profiling the mapping configuration 
+    /// </summary>
+    public class InventoryMappingProfile : Profile
+    {
+        public InventoryMappingProfile()
+        {
+            CreateMap<NewInventoryCommandRequest, InventoryDto>().ReverseMap();
+            CreateMap<Inventory, InventoryDto>().ReverseMap();
+        }
+    }
+}
